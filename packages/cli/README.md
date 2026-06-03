32291ea1518302a6299d7294012beab629e1c8ad
Ray-D-Song <songraysmail@gmail.com>
2026-06-03 09:36:43 +0800

    feat: cli default template

---
+++ packages/cli/README.md
@@ -0,0 +1,5 @@
+```bash
+npx create-raster-app my-app
+
+npx create-raster-app --platform=ios,android,windows,osx
+```
---
+++ packages/cli/templates/default/src/main.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { createRoot } from "raster/react";
+import { Button, Text, View } from "raster/components";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 12,
+      }}
+    >
+      <Text style={{ fontSize: 24 }}>Count: {count}</Text>
+      <Button onClick={() => setCount((value) => value + 1)}>Increment</Button>
+    </View>
+  );
+};
+
+const root = createRoot({
+  width: 800,
+  height: 600,
+});
+
+root.render(<App />);
